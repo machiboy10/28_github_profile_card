30a2fc9af49b2d510eceabf8444afd62b1003031
machilaptop <machicodes@gmail.com>
2022-02-15 04:31:21 +0800

    finalized

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # 28_github_profile_card
+[demo](https://machiboy10.github.io/28_github_profile_card/)
--- script.js
+++ script.js
@@ -1,1 +1,96 @@
+const APIURL = 'https://api.github.com/users/'
 
+const form = document.getElementById('form')
+const search = document.getElementById('search')
+const main = document.getElementById('main')
+
+// function getUser(username) {
+//     axios(APIURL + username)
+//         .then(res => console.log(res.data))
+//         .catch(err => console.log(err))
+// }
+
+async function getUser(username) {
+    //   const res = await axios(APIURL + username);
+    //   console.log(res.data)
+
+    try {
+        const {data} = await axios(APIURL + username)
+        createUserCard(data);
+        getRepos(username);
+        
+    } catch (err) {
+        //console.log(err);
+        if(err.response.status == 404){
+            createErrorCard('No profile with this username');
+        }
+      
+    }
+}   
+
+async function getRepos(username){
+    try {
+        const {data} = await axios(APIURL + 
+            username + '/repos?sort=created')
+            addReposToCard(data);
+        
+    } catch (err) {    
+            createErrorCard('Problem fetching repos');
+    }
+}
+
+function createUserCard(user){
+    const cardHTML = `<div class="card">
+    <div>
+      <img src="${user.avatar_url} alt=${user.name}" 
+      alt="" class="avatar">
+    </div>
+    <div class="user-info">
+      <h2>${user.name}</h2>
+      <p>${user.bio}</p>
+      <ul>
+        <li>${user.followers}<strong>Followers</strong></li>
+        <li>${user.following} <strong>Following</strong></li>
+        <li>${user.public_repos} <strong>Repos</strong></li>
+      </ul>
+      <div id="repos"></div>
+    </div>
+  </div>`;
+
+  main.innerHTML = cardHTML;
+
+}
+
+function createErrorCard(msg){
+    const cardHTML = `
+        <div class="card">
+            <h1>${msg}</h1>
+        </div>
+    `
+    main.innerHTML = cardHTML;
+}
+
+function addReposToCard(repos){
+    const reposEl = document.getElementById('repos');
+    repos
+    .slice(0,10)
+    .forEach(repo => {
+        const repoEl = document.createElement('a');
+        repoEl.classList.add('repo');
+        repoEl.href = repo.html_url;
+        repoEl.target = '_blank';
+        repoEl.innerText = repo.name;
+        reposEl.appendChild(repoEl);
+    })
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const user = search.value;
+
+    if(user){
+        getUser(user);
+        search.value = '';
+    }
+})
